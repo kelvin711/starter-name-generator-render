218f3b9c657136e48343e1fa0a5eac6dc6b71e3f
kelvin711 <arauzkelvin99@gmail.com>
2022-12-21 09:44:14 -0600

    ready for deployment

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react';
 
+const { REACT_APP_API_URL = "http://localhost:5001" } = process.env
+
 const App = () => {
   const [name, setName] = useState([]);
-
+  console.log(`${REACT_APP_API_URL}/generate`);
   useEffect(() => {
-    fetch('https://starter-name-generator.onrender.com/api/generate')
+    fetch(`${process.env.REACT_APP_API_URL}`)
       .then((res) => res.json())
       .then((nme) => setName([nme]));
   }, []);
