bda7c1386a43816fe462a948d444e358556ba390
kelvin711 <arauzkelvin99@gmail.com>
2022-12-21 10:12:07 -0600

    what a flub

--- client/src/App.js
+++ client/src/App.js
@@ -4,9 +4,9 @@ const { REACT_APP_API_URL = "http://localhost:5001" } = process.env
 
 const App = () => {
   const [name, setName] = useState([]);
-  console.log(`${REACT_APP_API_URL}/generate`);
+  console.log(`${REACT_APP_API_URL}generate`);
   useEffect(() => {
-    fetch(`${process.env.REACT_APP_API_URL}`)
+    fetch(`${process.env.REACT_APP_API_URL}/api/generate`)
       .then((res) => res.json())
       .then((nme) => setName([nme]));
   }, []);
